0af42675bd607b0422f395c667932f5779183c7e
RyanHeiser <rmheiser808@gmail.com>
2025-05-19 22:27:19 -0700

    initial

---
+++ NoteTaker/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoteTaker;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = true;
+    }
+
+    private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+    {
+        textEditor.Text = "";
+    }
+
+    private void exit_Click(object sender, RoutedEventArgs e)
+    {
+        this.Close();
+    }
+}
